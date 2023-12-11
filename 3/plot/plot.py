a37ffbfabf9cc05898b5526f1d2554ee70c52604
Lucrezia Bioni <lucrezia.bioni@gmail.com>
2023-12-11 12:28:36 +0100

    e/m - Added graph and conclusion

--- 3/plot/plot.py
+++ 3/plot/plot.py
@@ -42,15 +42,59 @@ s_y_ort = data_ort['s_y'].to_numpy()
 
 lr_ort = linear_regression(x_ort, y_ort, s_y_ort)
 
-x = np.linspace(min(x_ort)-1, max(x_ort)+1, 1000)
-def y(x):
-    return lr_ort['slope'] * x + lr_ort['inter']
+x1 = np.linspace(min(x_ort)-1, max(x_ort)+1, 1000)
+def y1(x1):
+    return lr_ort['slope'] * x1 + lr_ort['inter']
 
 plt.errorbar(x_ort, y_ort, s_y_ort, color="#A50021", ecolor="#FF4367", fmt='.', label="data")
-plt.plot(x, y(x), color="#0047A6", label="y = Ax + B")
+plt.plot(x1, y1(x1), color="#0047A6", label="y = Ax + B")
 plt.grid(True)
 plt.xlabel("2ΔV [V]")
 plt.ylabel("r²B² [N²/A²]")
 plt.title("Regressione lineare per la configurazione ortogonale")
 plt.legend()
 plt.savefig("graph_ort.png", dpi=240)
+plt.close()
+
+data_par = pd.read_csv('lr_parallelo.csv') # importa .csv come DataFrame
+x_par = data_par['x'].to_numpy() # trasforma colonna di DataFrame in array di numpy
+y_par = data_par['y'].to_numpy()
+s_y_par = data_par['s_y'].to_numpy()
+
+lr_par = linear_regression(x_par, y_par, s_y_par)
+
+x2 = np.linspace(min(x_par)-1, max(x_par)+1, 1000)
+def y2(x2):
+    return lr_par['slope'] * x2 + lr_par['inter']
+
+plt.errorbar(x_par, y_par, s_y_par, color="#A50021", ecolor="#FF4367", fmt='.', label="data")
+plt.plot(x2, y2(x2), color="#0047A6", label="y = Ax + B")
+plt.grid(True)
+plt.xlabel("2ΔV [V]")
+plt.ylabel("r²B² [N²/A²]")
+plt.title("Regressione lineare per la configurazione parallela")
+plt.legend()
+plt.savefig("graph_par.png", dpi=240)
+plt.close()
+
+
+data_apar = pd.read_csv('lr_antiparallelo.csv') # importa .csv come DataFrame
+x_apar = data_apar['x'].to_numpy() # trasforma colonna di DataFrame in array di numpy
+y_apar = data_apar['y'].to_numpy()
+s_y_apar = data_apar['s_y'].to_numpy()
+
+lr_apar = linear_regression(x_apar, y_apar, s_y_apar)
+
+x3 = np.linspace(min(x_apar)-1, max(x_apar)+1, 1000)
+def y3(x):
+    return lr_apar['slope'] * x + lr_apar['inter']
+
+plt.errorbar(x_apar, y_apar, s_y_apar, color="#A50021", ecolor="#FF4367", fmt='.', label="data")
+plt.plot(x3, y3(x3), color="#0047A6", label="y = Ax + B")
+plt.grid(True)
+plt.xlabel("2ΔV [V]")
+plt.ylabel("r²B² [N²/A²]")
+plt.title("Regressione lineare per la configurazione antiparallela")
+plt.legend()
+plt.savefig("graph_apar.png", dpi=240)
+plt.close()
