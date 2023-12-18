e461857e15ee390b90d9a2b2b5d77556518cf9f1
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2023-12-18 11:04:32 +0100

    Millikan - Modified analysis

--- 2/analysis/millikan.py
+++ 2/analysis/millikan.py
@@ -3,9 +3,10 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 dataset = pd.read_csv("charges.csv")
+dataset_err = pd.read_csv("charges_err.csv")
 
 charges = dataset['q'].to_numpy()
-charges = np.sort(charges)
+charges_err = dataset_err['err'].to_numpy()
 
 def S(q):
     sum = 0
@@ -13,11 +14,19 @@ def S(q):
         sum += (q - (Q / np.floor(0.5 + (Q / q))))**2
     return sum
 
-q = np.linspace(1.4e-19, 1.8e-19, 1000000)
+q = np.linspace(1.4e-19, 1.8e-19, 10000)
 
 plt.plot(q, S(q))
 plt.savefig("graph_py.png")
 
-x = np.arange(charges.shape[0])
-plt.plot(x, charges)
-plt.show()
+min = q[0]
+for i in range(len(q)):
+    if (S(q[i]) < S(min)):
+        min = q[i]
+print("min:", min)
+print("err-stat:", np.sqrt(S(min) / (len(charges) * (len(charges) - 1))))
+
+err = 0
+for i in range(len(charges)):
+    err += charges_err[i] / (len(charges) * np.floor(0.5 + (charges[i] / min)))
+print("err-sist:", err)
