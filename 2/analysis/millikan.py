496963e04c919a02ac81f24ccfa45c8bafd0e655
Leonardo Cerasi <leonardo.cerasi@outlook.it>
2024-01-03 10:39:28 +0100

    Millikan - Modified analysis+

--- 2/analysis/millikan.py
+++ 2/analysis/millikan.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-dataset = pd.read_csv("charges2.csv")
+dataset = pd.read_csv("charges-all.csv")
 
 charges = dataset['q'].to_numpy()
 charges_err = dataset['err'].to_numpy()
@@ -13,7 +13,28 @@ def S(q):
         sum += (q - (Q / np.floor(0.5 + (Q / q))))**2
     return sum
 
-q = np.linspace(1.4e-19, 1.8e-19, 10000)
+dict_d = {}
+
+for i in range(20, 101):
+    q_d = np.linspace(1.4e-19, 1.8e-19, i)
+    min_d = q_d[0]
+    for j in range(len(q_d)):
+        if (S(q_d[j]) < S(min_d)):
+            min_d = q_d[j]
+    e_d = 1.602176634e-19
+    perc_d = np.abs(e_d - min_d) / e_d
+    dict_d[i] = perc_d
+
+val_min = dict_d[20]
+index_min = 20
+for i in range(20, 101):
+    if dict_d[i] <= val_min:
+        val_min = dict_d[i]
+        index_min = i
+print('index:', index_min)
+print("")
+
+q = np.linspace(1.4e-19, 1.8e-19, index_min)
 
 plt.plot(q, S(q))
 plt.grid(True)
